ada2078afc2b1fcb183707d41b07cdde427fc15b
Denis Sidrikov <Denis_06.95@mail.ru>
2020-04-15 01:11:23 +0500

    Первый комит

---
+++ script/script.js
@@ -0,0 +1,28 @@
+"use strict"; 
+function funcMath () {
+    let a = 5;
+     return function newNumber(b){
+        while(isNaN(b)||b.trim()===''||b===null){
+            b = +newNumber(prompt ("Введи число!"));
+        }
+            if (b>a){
+                b = +newNumber(prompt ("Загаданное число меньше"));
+               }else if (b<a){
+                b = +newNumber(prompt("Загаданное число больше"));
+               }else {
+                   alert("Ты угадал! Игра окончена!");
+               } 
+    };
+}
+const mathPow = funcMath();
+let y;
+let start = function(){
+    y = prompt("Угадай число от 1 до 100");
+    while (isNaN(y)||y.trim()===''||y===null){
+        y=prompt("Угадай число от 1 до 100");
+    }
+    return y;
+};
+start();
+mathPow(y);
+
