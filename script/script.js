c37252e342e51ea0a4181f5d5020ad26d1ac4427
Denis Sidrikov <Denis_06.95@mail.ru>
2020-04-15 23:10:59 +0500

    Навел красоту

--- script/script.js
+++ script/script.js
@@ -1,30 +1,28 @@
-"use strict"; 
+"use strict";
 
-
-function funcMath () {
-    let a = Math.floor(Math.random() * 10);
-     return function newNumber(b){
-        while(isNaN(b)||b.trim()===''||b===null){
-            b = +newNumber(prompt ("Введи число!"));
+function funcMath() {
+    let a = Math.floor(Math.random() * 100);
+    return function newNumber(b) {
+        while (isNaN(b) || b.trim() === '' || b === null) {
+            b = +newNumber(prompt("Введи число!"));
         }
-            if (b>a){
-                b = +newNumber(prompt ("Загаданное число меньше"));
-               }else if (b<a){
-                b = +newNumber(prompt("Загаданное число больше"));
-               }else {
-                   alert("Ты угадал! Игра окончена!");
-               } 
+        if (b > a) {
+            b = +newNumber(prompt("Загаданное число меньше"));
+        } else if (b < a) {
+            b = +newNumber(prompt("Загаданное число больше"));
+        } else {
+            alert("Ты угадал! Игра окончена!");
+        }
     };
 }
 const mathPow = funcMath();
 let y;
-let start = function(){
+let start = function () {
     y = prompt("Угадай число от 1 до 100");
-    while (isNaN(y)||y.trim()===''||y===null){
-        y=prompt("Введи число!");
+    while (isNaN(y) || y.trim() === '' || y === null) {
+        y = prompt("Введи число!");
     }
     return y;
 };
 start();
 mathPow(y);
-
